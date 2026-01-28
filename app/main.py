dab2b9219306fd231c70c237d2fc6a783ec2a63a
Anthony Wright <anthony@mrwrite.dev>
2026-01-28 06:58:49 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ else:
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://topstep-mvp-bot.vercel.app/"
     ]
 
 app.add_middleware(
